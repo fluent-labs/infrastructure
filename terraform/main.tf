8ea11eb23e8d0efca581147589e1779c1d3b5dbd
Lucas Kjaero <lucas@lucaskjaero.com>
2020-12-23 22:59:14 -0800

    No more container registry

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,11 +37,6 @@ provider "helm" {
   }
 }
 
-# Service container registries
-module "container_registries" {
-  source = "./container_registries"
-}
-
 # Mysql database to store user context.
 # module "database" {
 #   source       = "./database"
