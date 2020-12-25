52409b96a61a329bd4193f2796ed3b1f9767f1bb
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2020-12-25 01:11:15 -0800

    S3 bucket for spark (#25)
    * Upgrade spark operator to new version
    
    * Rename the bucket for more types of content
    
    * Spark IAM user
    
    * Permissions for reading content bucket

--- terraform/content/main.tf
+++ terraform/content/main.tf
@@ -14,9 +14,9 @@ resource "kubernetes_namespace" "content" {
 
 resource "helm_release" "spark" {
   name       = "spark"
-  repository = "http://storage.googleapis.com/kubernetes-charts-incubator"
+  repository = "https://googlecloudplatform.github.io/spark-on-k8s-operator"
   chart      = "sparkoperator"
-  version    = "0.6.9"
+  version    = "1.0.5"
   namespace  = "content"
 
   depends_on = [
@@ -26,10 +26,58 @@ resource "helm_release" "spark" {
 
 # Content buckets for spark to read
 
-resource "aws_s3_bucket" "definitions" {
-  bucket = "foreign-language-reader-definitions"
+resource "aws_s3_bucket" "content" {
+  bucket = "foreign-language-reader-content"
   acl    = "private"
 }
+
+# S3 Credentials for Spark
+
+resource "aws_iam_access_key" "spark" {
+  user = aws_iam_user.spark.name
+}
+
+resource "aws_iam_user" "spark" {
+  name = "spark"
+}
+
+resource "kubernetes_secret" "spark_s3_creds" {
+  metadata {
+    name      = "spark-s3-creds"
+    namespace = "content"
+  }
+
+  data = {
+    access_key = aws_iam_access_key.spark.id
+    secret_key = aws_iam_access_key.spark.secret
+  }
+
+  depends_on = [
+    kubernetes_namespace.content
+  ]
+}
+
+data "aws_iam_policy_document" "spark_read" {
+  statement {
+    actions   = ["s3:GetBucketLocation", "s3:GetObject", "s3:ListBucket"]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.content.arn}/*"]
+  }
+}
+
+resource "aws_iam_policy" "spark_read" {
+  name        = "spark-read-role"
+  description = "IAM policy to let spark read from content uploaded to S3"
+
+  policy = data.aws_iam_policy_document.spark_read.json
+}
+
+resource "aws_iam_policy_attachment" "spark_read" {
+  name       = "spark-read-role-attach"
+  users      = [aws_iam_user.spark.name]
+  policy_arn = aws_iam_policy.spark_read.arn
+}
+
 
 # resource "helm_release" "zeppelin" {
 #   name       = "zeppelin"
