76472af485914036a57f02b43202f12c86b89ace
Lucas Kjaero <lucas@lucaskjaero.com>
2021-01-01 22:58:38 -0800

    Passwords are sensitive

--- terraform/elasticsearch/variables.tf
+++ terraform/elasticsearch/variables.tf
@@ -1,13 +1,16 @@
 variable "api_password" {
   description = "The size machine to run the database on."
+  sensitive   = true
 }
 
 variable "fluentd_password" {
   description = "The password for the logging user."
+  sensitive   = true
 }
 
 variable "spark_password" {
   description = "The password for the spark user."
+  sensitive   = true
 }
 
 variable "domain" {
