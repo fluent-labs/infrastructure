bd8d112667e83f10c03864356b0bcf4c0637402a
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-27 14:27:21 -0800

    Increase default TTL to a year

--- terraform/static_bucket/main.tf
+++ terraform/static_bucket/main.tf
@@ -88,7 +88,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
-    max_ttl                = 86400
+    max_ttl                = 31536000
   }
 
   restrictions {
