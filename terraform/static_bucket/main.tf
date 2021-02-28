4617adeebe31f2cf99003afbdc4029390d256c23
Lucas Kjaero <lucas@lucaskjaero.com>
2021-02-28 00:57:38 -0800

    Gzip content when possible

--- terraform/static_bucket/main.tf
+++ terraform/static_bucket/main.tf
@@ -75,7 +75,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.full_domain
 
     forwarded_values {
@@ -87,8 +87,28 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
+    default_ttl            = 86400
     max_ttl                = 31536000
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "*"
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.full_domain
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
   }
 
   restrictions {
