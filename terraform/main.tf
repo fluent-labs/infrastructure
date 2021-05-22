6a83f2c2410936c8e145dfda1a0db0962fd863af
Lucas Kjaero <lucas@lucaskjaero.com>
2021-05-22 19:10:23 +0200

    No config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,6 @@ data "digitalocean_kubernetes_cluster" "foreign_language_reader" {
 }
 
 provider "kubernetes" {
-  load_config_file = false
   host             = data.digitalocean_kubernetes_cluster.foreign_language_reader.endpoint
   token            = data.digitalocean_kubernetes_cluster.foreign_language_reader.kube_config[0].token
   cluster_ca_certificate = base64decode(
