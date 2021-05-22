3b9c2a5ce46b0c4c90d8e4b7ed37f0b200b50652
Lucas Kjaero <lucas@lucaskjaero.com>
2021-05-22 09:30:16 +0200

    Sure

--- terraform/main.tf
+++ terraform/main.tf
@@ -159,14 +159,13 @@ resource "digitalocean_domain" "fluentlabs" {
 }
 
 # Frontend deploy user
-# test comment
-# resource "aws_iam_access_key" "github" {
-#   user = aws_iam_user.github.name
-# }
+resource "aws_iam_access_key" "github" {
+  user = aws_iam_user.github.name
+}
 
-# resource "aws_iam_user" "github" {
-#   name = "foreign-language-reader-github"
-# }
+resource "aws_iam_user" "github" {
+  name = "foreign-language-reader-github"
+}
 
 # TLS
 
