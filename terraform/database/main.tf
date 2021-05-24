da4116a31b57c31a55967ea54efa5772c1f4575b
Lucas Kjaero <lucas@lucaskjaero.com>
2021-05-24 10:10:55 +0200

    Correct version number

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -4,15 +4,16 @@ resource "random_password" "database_admin_password" {
 }
 
 resource "aws_db_instance" "fluentlabs" {
-  engine               = "postgres"
-  engine_version       = "PostgreSQL 12.5-R1"
-  instance_class       = "db.t3.micro"
-  name                 = "fluentlabs"
-  username             = "admin"
-  password             = random_password.database_admin_password.result
-  parameter_group_name = "default.postgres12"
-  skip_final_snapshot  = true
-  deletion_protection  = true
+  engine                     = "postgres"
+  engine_version             = "12.5"
+  instance_class             = "db.t3.micro"
+  name                       = "fluentlabs"
+  username                   = "admin"
+  password                   = random_password.database_admin_password.result
+  parameter_group_name       = "default.postgres12"
+  skip_final_snapshot        = true
+  deletion_protection        = true
+  auto_minor_version_upgrade = true
 
   # Storage autoscaling goes here
   allocated_storage     = 20
