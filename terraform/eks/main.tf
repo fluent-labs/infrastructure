6a93054466a1c246eb722ac3b5f9474b66ccdc87
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-18 12:08:17 +0200

    vpc

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,3 +1,9 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
 # Permissions for K8s cluster go below here.
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
