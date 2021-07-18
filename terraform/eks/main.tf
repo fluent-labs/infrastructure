670b460dcff7c28215b164738ed40c1ad999f26c
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2021-07-18 14:47:39 -0700

    Add container read policy to EKS (#91)
    * Add container read policy to EKS
    
    * Explanatory comment

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -91,6 +91,12 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy_worker" {
   role       = aws_iam_role.service_workers.name
 }
 
+# This policy is mandatory from AWS so don't remove it.
+resource "aws_iam_role_policy_attachment" "eks_container_read_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.service_workers.name
+}
+
 resource "aws_eks_node_group" "services" {
   cluster_name    = aws_eks_cluster.fluentlabs.name
   node_group_name = "services"
@@ -106,5 +112,6 @@ resource "aws_eks_node_group" "services" {
   depends_on = [
     aws_iam_role_policy_attachment.eks_worker_node_policy,
     aws_iam_role_policy_attachment.eks_cni_policy_worker,
+    aws_iam_role_policy_attachment.eks_container_read_policy,
   ]
 }
