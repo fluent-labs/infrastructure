feb40a64d47b96389601d7a3274ff0a82f443c9c
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-19 00:40:17 +0200

    fmt

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -11,9 +11,9 @@ data "aws_availability_zones" "available" {
 resource "aws_subnet" "public" {
   count = 3
 
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  vpc_id                  = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  vpc_id            = aws_vpc.main.id
 
   # Required so that the nodes can join the kubernetes cluster
   map_public_ip_on_launch = true
