14aad55260d481cd1d8089deeaf400605aaaac09
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-19 00:37:51 +0200

    Make all the subnets public

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -11,9 +11,10 @@ data "aws_availability_zones" "available" {
 resource "aws_subnet" "public" {
   count = 3
 
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  vpc_id            = aws_vpc.main.id
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
 
   tags = {
     Name                                    = "fluentlabs-public-${count.index}"
