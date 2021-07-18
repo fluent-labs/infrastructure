710eb244f01443749cb8d8cb26442f222ed90ace
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-18 12:11:35 +0200

    Subnets

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -4,6 +4,33 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 }
 
+resource "aws_subnet" "public_one" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "fluentlabs-public-1"
+  }
+}
+
+resource "aws_subnet" "public_two" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "fluentlabs-public-2"
+  }
+}
+
+resource "aws_subnet" "public_three" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "fluentlabs-public-3"
+  }
+}
+
 # Permissions for K8s cluster go below here.
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
