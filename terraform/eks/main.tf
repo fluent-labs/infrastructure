d775d6b7884ef8790db8cf7f909ced610147a325
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-26 00:14:21 +0200

    Name our vpc

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -2,6 +2,10 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "fluentlabs-prod"
+  }
 }
 
 data "aws_availability_zones" "available" {
