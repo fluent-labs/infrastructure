1b104d1c2f6ebc255275b54824adde3817bb9356
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-26 00:38:54 +0200

    fmt

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -23,7 +23,7 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name                                                       = "fluentlabs-public-${count.index}"
+    Name                                   = "fluentlabs-public-${count.index}"
     "kubernetes.io/cluster/fluentlabsprod" = "shared"
   }
 }
