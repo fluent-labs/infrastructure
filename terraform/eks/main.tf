602fbf3a70616ae427bbb45511982fd44e500fc3
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-31 16:37:39 +0200

    Connect to the internet

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -8,6 +8,14 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "fluentlabs-prod"
+  }
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
