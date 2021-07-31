dc1ea9c3b3c3d924b974c240236a00af013111c6
Lucas Kjaero <lucas@lucaskjaero.com>
2021-08-01 00:23:25 +0200

    Missed a spot

--- terraform/elasticsearch/main.tf
+++ terraform/elasticsearch/main.tf
@@ -117,18 +117,18 @@ resource "kubernetes_secret" "elasticsearch_roles" {
 #   }
 # }
 
-resource "digitalocean_record" "kibana_subdomain_dns" {
-  domain = var.domain
-  type   = "A"
-  name   = "kibana"
-  value  = data.kubernetes_service.kibana.status.0.load_balancer.0.ingress.0.ip
+# resource "digitalocean_record" "kibana_subdomain_dns" {
+#   domain = var.domain
+#   type   = "A"
+#   name   = "kibana"
+#   value  = data.kubernetes_service.kibana.status.0.load_balancer.0.ingress.0.ip
 
-  lifecycle {
-    ignore_changes = [
-      value,
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       value,
+#     ]
+#   }
+# }
 
 # Backups in S3
 
