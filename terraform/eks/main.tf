b69885ca4d2cd063f3c0d4cf655ab5f4eaf53f73
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-31 12:40:07 +0200

    Set up cluster addon

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -75,6 +75,21 @@ resource "aws_eks_cluster" "fluentlabs" {
   ]
 }
 
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = aws_eks_cluster.fluentlabs.name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = aws_eks_cluster.fluentlabs.name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name = aws_eks_cluster.fluentlabs.name
+  addon_name   = "coredns"
+}
+
 resource "aws_iam_role" "service_workers" {
   name = "eks-node-group-service"
 
