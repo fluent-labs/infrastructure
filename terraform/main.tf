6f83fdffbfa893036bf0a305b54a38cb0d357bbb
Lucas Kjaero <lucas@lucaskjaero.com>
2021-08-01 00:12:21 +0200

    No config file

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,23 +38,21 @@ data "aws_eks_cluster_auth" "main" {
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
-  load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
 }
 
 provider "kubernetes-alpha" {
   host                   = data.aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.main.token
-  load_config_file       = false
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.main.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.main.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
   }
 }
 
