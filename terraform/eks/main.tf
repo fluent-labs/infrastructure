86c3d6cd73efc03070a9e03db536ed7a627d182a
Lucas Kjaero <lucas@lucaskjaero.com>
2021-07-31 16:58:32 +0200

    Add a route table

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -16,6 +16,19 @@ resource "aws_internet_gateway" "gateway" {
   }
 }
 
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway.id
+  }
+
+  tags = {
+    Name = "fluentlabs-prod"
+  }
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
