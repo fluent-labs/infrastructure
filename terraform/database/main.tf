e817955bb917e0f707e455cd5cc6fe31dc3ed66a
Lucas Kjaero <lucas@lucaskjaero.com>
2021-08-15 12:17:00 +0200

    Organize RDS config into sections

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -13,21 +13,30 @@ resource "aws_db_subnet_group" "main" {
 }
 
 resource "aws_db_instance" "fluentlabs" {
-  engine                     = "postgres"
-  engine_version             = "12.5"
-  instance_class             = "db.t3.micro"
-  identifier                 = "fluentlabs"
-  name                       = "fluentlabs"
-  username                   = "fluentlabs_admin"
-  password                   = random_password.database_admin_password.result
-  parameter_group_name       = "default.postgres12"
+  // Basic config
+  engine               = "postgres"
+  engine_version       = "12.5"
+  instance_class       = "db.t3.micro"
+  identifier           = "fluentlabs"
+  name                 = "fluentlabs"
+  parameter_group_name = "default.postgres12"
+
+  // Backups and deletion protection
   skip_final_snapshot        = true
-  aws_db_subnet_group        = aws_db_subnet_group.main.name
   deletion_protection        = true
   auto_minor_version_upgrade = true
   backup_retention_period    = 30
 
-  # Storage autoscaling goes here
+  // Security goes here
+  // This is fine because we have tight security groups
+  username            = "fluentlabs_admin"
+  password            = random_password.database_admin_password.result
+  publicly_accessible = true
+
+  // Networking
+  aws_db_subnet_group = aws_db_subnet_group.main.name
+
+  # Storage autoscaling
   allocated_storage     = 20
   max_allocated_storage = 100
 }
