a695e201560557423a912bd7f857f9b4887a2e4f
Lucas Kjaero <lucas@lucaskjaero.com>
2021-08-15 12:01:24 +0200

    API can connect to database

--- terraform/database/main.tf
+++ terraform/database/main.tf
@@ -3,6 +3,15 @@ resource "random_password" "database_admin_password" {
   special = true
 }
 
+resource "aws_db_subnet_group" "main" {
+  name       = "fluentlabs-kubernetes-subnets"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = "Fluentlabs database subnet group"
+  }
+}
+
 resource "aws_db_instance" "fluentlabs" {
   engine                     = "postgres"
   engine_version             = "12.5"
@@ -13,6 +22,7 @@ resource "aws_db_instance" "fluentlabs" {
   password                   = random_password.database_admin_password.result
   parameter_group_name       = "default.postgres12"
   skip_final_snapshot        = true
+  aws_db_subnet_group        = aws_db_subnet_group.main.name
   deletion_protection        = true
   auto_minor_version_upgrade = true
   backup_retention_period    = 30
