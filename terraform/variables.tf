02b9aa8fc993ab4bbffb30d911d930e533223eb9
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-09 15:24:32 +0200

    Missing reference

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,6 +10,6 @@ variable "sematext_index_name" {
   description = "The elasticsearch index to send logs to. This is kept secret."
 }
 
-variable "subnet_ids" {
-  description = "The subnet IDs for the kubernetes cluster"
-}
+# variable "subnet_ids" {
+#   description = "The subnet IDs for the kubernetes cluster"
+# }
