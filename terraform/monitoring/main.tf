43cd686f0b0b1a22903f38c0c12ffc30230b71a5
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-09 16:05:50 +0200

    Kill a couple more helm charts

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -16,43 +16,43 @@ resource "random_password" "fluent_elasticsearch_password" {
   min_numeric = 10
 }
 
-resource "helm_release" "fluentd_elasticsearch" {
-  name       = "fluentd"
-  repository = "https://kokuwaio.github.io/helm-charts"
-  chart      = "fluentd-elasticsearch"
-  version    = "11.6.2"
+# resource "helm_release" "fluentd_elasticsearch" {
+#   name       = "fluentd"
+#   repository = "https://kokuwaio.github.io/helm-charts"
+#   chart      = "fluentd-elasticsearch"
+#   version    = "11.6.2"
 
-  values = [file("${path.module}/fluentd.yml")]
+#   values = [file("${path.module}/fluentd.yml")]
 
-  set_sensitive {
-    name  = "elasticsearch.auth.password"
-    value = random_password.fluent_elasticsearch_password.result
-  }
-}
+#   set_sensitive {
+#     name  = "elasticsearch.auth.password"
+#     value = random_password.fluent_elasticsearch_password.result
+#   }
+# }
 
-resource "helm_release" "prometheus_operator" {
-  name       = "prometheus"
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "kube-prometheus"
-  version    = "3.3.2"
-}
+# resource "helm_release" "prometheus_operator" {
+#   name       = "prometheus"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "kube-prometheus"
+#   version    = "3.3.2"
+# }
 
-resource "helm_release" "sematext_logagent" {
-  name       = "sematext"
-  repository = "https://cdn.sematext.com/helm-charts"
-  chart      = "sematext-agent"
-  version    = "1.0.35"
+# resource "helm_release" "sematext_logagent" {
+#   name       = "sematext"
+#   repository = "https://cdn.sematext.com/helm-charts"
+#   chart      = "sematext-agent"
+#   version    = "1.0.35"
 
-  set {
-    name  = "region"
-    value = "US"
-  }
+#   set {
+#     name  = "region"
+#     value = "US"
+#   }
 
-  set_sensitive {
-    name  = "logsToken"
-    value = var.sematext_index_name
-  }
-}
+#   set_sensitive {
+#     name  = "logsToken"
+#     value = var.sematext_index_name
+#   }
+# }
 
 # resource "kubernetes_manifest" "api_prometheus" {
 #   provider = kubernetes-alpha
