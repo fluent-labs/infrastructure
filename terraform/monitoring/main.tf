33a387a3520392f85fb2400694fa29f808a088f1
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-10 15:22:40 +0200

    Rearrange the things

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -9,33 +9,6 @@ resource "helm_release" "metrics_server" {
 # Logging configuration
 # Every node has a log collection agent that posts logs to elasticsearch
 
-resource "random_password" "fluent_elasticsearch_password" {
-  length      = 32
-  special     = false
-  min_numeric = 10
-}
-
-# resource "helm_release" "fluentd_elasticsearch" {
-#   name       = "fluentd"
-#   repository = "https://kokuwaio.github.io/helm-charts"
-#   chart      = "fluentd-elasticsearch"
-#   version    = "11.6.2"
-
-#   values = [file("${path.module}/fluentd.yml")]
-
-#   set_sensitive {
-#     name  = "elasticsearch.auth.password"
-#     value = random_password.fluent_elasticsearch_password.result
-#   }
-# }
-
-# resource "helm_release" "prometheus_operator" {
-#   name       = "prometheus"
-#   repository = "https://charts.bitnami.com/bitnami"
-#   chart      = "kube-prometheus"
-#   version    = "3.3.2"
-# }
-
 resource "helm_release" "sematext_logagent" {
   name       = "sematext"
   repository = "https://cdn.sematext.com/helm-charts"
@@ -52,6 +25,15 @@ resource "helm_release" "sematext_logagent" {
     value = var.sematext_index_name
   }
 }
+
+# Monitoring from Prometheus
+
+# resource "helm_release" "prometheus_operator" {
+#   name       = "prometheus"
+#   repository = "https://charts.bitnami.com/bitnami"
+#   chart      = "kube-prometheus"
+#   version    = "3.3.2"
+# }
 
 # resource "kubernetes_manifest" "api_prometheus" {
 #   provider = kubernetes-alpha
