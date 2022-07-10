000d25b7202bc4956f0a7a157b926955a36794bc
Lucas Kjaero-Zhang <lucaskjaero@users.noreply.github.com>
2022-07-10 06:25:56 -0700

    Set up log shipping (#154)
    It'll be good to get logs from what's happening

--- terraform/monitoring/main.tf
+++ terraform/monitoring/main.tf
@@ -9,48 +9,30 @@ resource "helm_release" "metrics_server" {
 # Logging configuration
 # Every node has a log collection agent that posts logs to elasticsearch
 
-resource "random_password" "fluent_elasticsearch_password" {
-  length      = 32
-  special     = false
-  min_numeric = 10
+resource "helm_release" "sematext_logagent" {
+  name       = "sematext"
+  repository = "https://cdn.sematext.com/helm-charts"
+  chart      = "sematext-agent"
+  version    = "1.0.46"
+
+  set {
+    name  = "region"
+    value = "US"
+  }
+
+  set_sensitive {
+    name  = "infraToken"
+    value = var.sematext_index_name
+  }
 }
 
-# resource "helm_release" "fluentd_elasticsearch" {
-#   name       = "fluentd"
-#   repository = "https://kokuwaio.github.io/helm-charts"
-#   chart      = "fluentd-elasticsearch"
-#   version    = "11.6.2"
-
-#   values = [file("${path.module}/fluentd.yml")]
-
-#   set_sensitive {
-#     name  = "elasticsearch.auth.password"
-#     value = random_password.fluent_elasticsearch_password.result
-#   }
-# }
+# Monitoring from Prometheus
 
 # resource "helm_release" "prometheus_operator" {
 #   name       = "prometheus"
 #   repository = "https://charts.bitnami.com/bitnami"
 #   chart      = "kube-prometheus"
 #   version    = "3.3.2"
-# }
-
-# resource "helm_release" "sematext_logagent" {
-#   name       = "sematext"
-#   repository = "https://cdn.sematext.com/helm-charts"
-#   chart      = "sematext-agent"
-#   version    = "1.0.35"
-
-#   set {
-#     name  = "region"
-#     value = "US"
-#   }
-
-#   set_sensitive {
-#     name  = "logsToken"
-#     value = var.sematext_index_name
-#   }
 # }
 
 # resource "kubernetes_manifest" "api_prometheus" {
