c896fe55f5776b0262047526639b3b3c0d6848a4
Lucas Kjaero <lucas@lucaskjaero.com>
2022-07-17 22:28:54 +0200

    Role from operator

--- terraform/jobs/main.tf
+++ terraform/jobs/main.tf
@@ -11,3 +11,61 @@ resource "helm_release" "jenkins" {
   version    = "0.6.2"
   values     = [file("${path.module}/jenkins.yml")]
 }
+
+resource "kubernetes_role" "jenkins" {
+  metadata {
+    name      = "jenkins-operator-jenkins"
+    namespace = "jobs"
+  }
+
+  rule {
+    api_groups = [""]
+    resources  = ["pods/portforward"]
+    verbs      = ["create"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods"]
+    verbs      = ["create", "delete", "get", "list", "patch", "update", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods/exec"]
+    verbs      = ["create", "delete", "get", "list", "patch", "update", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["configmaps"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["pods/log"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["secrets"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = [""]
+    resources  = ["events"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = ["image.openshift.io"]
+    resources  = ["imagestreams"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = ["build.openshift.io"]
+    resources  = ["buildconfigs"]
+    verbs      = ["get", "list", "watch"]
+  }
+  rule {
+    api_groups = ["build.openshift.io"]
+    resources  = ["builds"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
